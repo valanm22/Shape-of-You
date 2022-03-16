1d877fb55440fb0b12dac985484767f3bac9b511
Valan Baptist Mathuranayagam <valanm22@gmail.com>
2022-03-16 22:51:44 +0530

    Add paintComponent

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
+import java.awt.Graphics;
 
 public class Drawing extends JPanel {
 
     private Shape shape;
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
 }
